fd13d415bc1715273b86c6d66af0983f663bb94f
Nick Ulle <nick.ulle@gmail.com>
2017-04-21 16:25:14 -0700

    Update feature scripts.

--- src/features/01_nick_features.R
+++ src/features/01_nick_features.R
@@ -15,6 +15,7 @@ test_out = test["lineID"]
 
 
 # total_units ----------------------------------------
+message("total_units")
 # `content` split on "X" and multiplied.
 
 # Some rows have format ##X##X##.
@@ -31,11 +32,13 @@ total_units = split_units[, 1] * split_units[, 2] * split_units[, 3]
 
 
 # rrp_per_unit ----------------------------------------
+message("rrp_per_unit")
 
 rrp_per_unit = items$rrp / total_units
 
 
 # price_per_unit ----------------------------------------
+message("price_per_unit")
 i = match(train$pid, items$pid)
 j = match(test$pid, items$pid)
 
@@ -44,9 +47,17 @@ test_price_per_unit = test$price / total_units[j]
 
 
 # competitorprice_per_unit ----------------------------------------
+message("competitor_price_per_unit")
 
-train_competitorprice_per_unit = train$competitorPrice / total_units[i]
-test_competitorprice_per_unit = test$competitorPrice / total_units[j]
+train_competitor_price_per_unit = train$competitorPrice / total_units[i]
+test_competitor_price_per_unit = test$competitorPrice / total_units[j]
+
+
+# day_of_week ----------------------------------------
+message("day_of_week")
+
+train_day_of_week = train$day %% 7
+test_day_of_week = test$day %% 7
 
 
 # OUTPUT ----------------------------------------
@@ -56,9 +67,13 @@ items_out$rrp_per_unit = rrp_per_unit
 write_csv(test_out, "data/interim/items_rrp_per_unit.csv")
 
 train_out$price_per_unit = train_price_per_unit
-train_out$competitor_price_per_unit = train_competitorprice_per_unit
+train_out$competitor_price_per_unit = train_competitor_price_per_unit
+train_out$day_of_week = train_day_of_week
 write_csv(train_out, "data/interim/train_price_per_unit.csv")
 
 test_out$price_per_unit = test_price_per_unit
-test_out$competitor_price_per_unit = test_competitorprice_per_unit
+test_out$competitor_price_per_unit = test_competitor_price_per_unit
+test_out$day_of_week = test_day_of_week
 write_csv(test_out, "data/interim/test_price_per_unit.csv")
+
+message("\nAll features saved!")
