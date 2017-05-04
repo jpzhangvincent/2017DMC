660d5d17e8074cea98edce6b6152b225368e51e6
jpzhangvincent <vincentzhangv@gmail.com>
2017-05-04 13:57:46 -0700

    rough time-order cv training

---
+++ src/models/cvtrain_example.R
@@ -0,0 +1,103 @@
+library(feather)
+library(dplyr)
+library(xgboost)
+options(dplyr.width = Inf)
+library(h2o)
+
+
+#----------------------setting cross validation -----------------------------
+train_set <- read_feather("data/processed/training_set.feather") 
+# create a weight column based on the quantity of items bought
+train_set <- train_set %>% mutate(num_items_bought = revenue/price) %>% 
+  mutate(weight_quantity = ifelse(num_items_bought==0, 
+                                  num_items_bought+1, num_items_bought)) %>% 
+  mutate(lineID = as.integer(lineID)) %>% arrange(lineID)
+# the weight column is useful for weighted training to specify in the h2o's models
+
+get_fold_number = function(x){
+  if(x %in% 1:15)
+    return(1)
+  else if(x %in% 16:31)
+    return(2)
+  else if(x %in% 32:46)
+    return(3)
+  else if(x %in% 47:61)
+    return(4)
+  else
+    return(5)
+}
+train_set <- train_set %>% mutate(fold_indicator = sapply(day, get_fold_number))
+
+
+#----------------------prepare training data frame--------------------------
+h2o.init(nthreads = -1, #Number of threads -1 means use all cores on your machine
+         max_mem_size = "8G")  #max mem size is the maximum memory to allocate to H2O
+train_set.hex <- as.h2o(train_set, destination_frame = "train_set.hex")
+# factorize the categorical variables
+train_set.hex$order <- as.factor(train_set.hex$order)
+train_set.hex$manufacturer <- as.factor(train_set.hex$manufacturer)
+train_set.hex$pharmForm <- as.factor(train_set.hex$pharmForm)
+train_set.hex$group <- as.factor(train_set.hex$group)
+train_set.hex$unit <- as.factor(train_set.hex$unit)
+train_set.hex$category <- as.factor(train_set.hex$category)
+train_set.hex$campaignIndex <- as.factor(train_set.hex$campaignIndex)
+train_set.hex$salesIndex <- as.factor(train_set.hex$salesIndex)
+train_set.hex$adFlag <- as.factor(train_set.hex$adFlag)
+train_set.hex$last_adFlag <- as.factor(train_set.hex$last_adFlag)
+train_set.hex$availability <- as.factor(train_set.hex$availability)
+train_set.hex$last_avaibility <- as.factor(train_set.hex$last_avaibility)
+train_set.hex$group_beginNum <- as.factor(train_set.hex$group_beginNum)
+train_set.hex$genericProduct <- as.factor(train_set.hex$genericProduct)
+train_set.hex$content <- as.factor(train_set.hex$content)
+train_set.hex$avaibility_transition <- as.factor(train_set.hex$avaibility_transition)
+train_set.hex$adFlag_transition <- as.factor(train_set.hex$adFlag_transition)
+
+# response and predictors
+y <- "order"
+predictors <- setdiff(names(train_set), c("pid", "lineID", "day","order", "basket", "click", "revenue",
+                                          "num_items_bought", "weight_quantity", "fold_indicator", 
+                                          "content_part1", "content_part2", "content_part3"))
+print(predictors)
+
+# Train & Cross-validate a GBM
+gbm1 <- h2o.gbm(x = predictors,
+                y = y,
+                training_frame = train_set.hex,
+                distribution = "bernoulli",
+                ntrees = 500,
+                max_depth = 8,
+                col_sample_rate_per_tree = 0.9,
+                min_rows = 3,
+                learn_rate = 0.05,
+                fold_column = "fold_indicator",
+                stopping_metric = "AUC",
+                stopping_rounds = 5,
+                weights_column = "weight_quantity",
+                keep_cross_validation_predictions = TRUE,
+                seed = 1)
+print(gbm1)
+h2o.varimp(gbm1)
+plot(gbm1)
+h2o.saveModel(gbm1, "models/allfeatures_auc_gbm1_0.657")
+
+gbm2 <-  h2o.gbm(x = predictors,
+                     y = y,
+                     training_frame = train_set.hex,
+                     distribution = "bernoulli",
+                     ntrees = 500,
+                     max_depth = 8,
+                     col_sample_rate_per_tree = 0.9,
+                     min_rows = 5,
+                     learn_rate = 0.05,
+                     fold_column = "fold_indicator",
+                     stopping_metric = "misclassification",
+                     stopping_rounds = 5,
+                     weights_column = "weight_quantity",
+                     keep_cross_validation_predictions = TRUE,
+                     seed = 1)
+print(gbm2)
+summary(gbm2)
+plot(gbm2)
+h2o.saveModel(gbm2, "models/allfeatures_misclass_xgb_0.66")
+head(as.data.frame(h2o.varimp(gbm2)))
+h2o.varimp(gbm2)
