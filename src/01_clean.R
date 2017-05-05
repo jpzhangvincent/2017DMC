ce7c36644a0d2d166dab05d79ee66c75fd88378c
Nick Ulle <nick.ulle@gmail.com>
2017-05-05 01:09:18 -0700

    Add steps 01-02 of pipeline.
    This commit adds two scripts to clean the data and add row-wise
    features, respectively.

--- src/01_clean.R
+++ src/01_clean.R
@@ -1,26 +1,71 @@
+#!/usr/bin/env Rscript
+#
+# This script cleans the data sets without adding any new features.
+#
+
 library(data.table)
-library(tidyverse)
-library(stringi)
+library(feather)
+library(stringr)
 
-# read data and treat empty strings as NA
-train_df <- fread('../data/raw/train.csv', na.strings = c('', ' ', NA))
-item_df <- fread('../data/raw/items.csv', na.strings = c('', ' ', NA)) 
-setkey(train_df, pid)
-setkey(item_df, pid)
+IN <- list(
+  items = "../data/raw/items.csv",
+  train = "../data/raw/train.csv",
+  test  = "../data/raw/class.csv")
 
-orig_train_df <- merge(train_df, item_df, all.x = TRUE)
+OUT <- list(
+  items = "../data/interim/01_clean_items.feather",
+  train = "../data/interim/01_clean_train.feather",
+  test  = "../data/interim/01_clean_test.feather")
 
-# convert categorical variables as string type
-to_string_vars <- c("pid", "lineID", "manufacturer", "group", "content", "unit", 
-                    "pharmForm", "genericProduct", "salesIndex", "category", "campaignIndex")
-orig_train_df <- orig_train_df[, (to_string_vars) := lapply(.SD, as.character), .SDcols=to_string_vars]
+NA_STRINGS <- c("", " ", "NA")
 
-# lowercase string to be consistent
-orig_train_df[, `:=`(group = tolower(group), pharmForm = tolower(pharmForm),
-                unit = tolower(unit))]
 
-# get quantity 
-orig_train_df[, num_items_bought := as.integer(revenue/price)]
+# ==================== ITEMS ====================
+if (!file.exists(OUT$items)) {
+  items <- fread(IN$items, na.strings = NA_STRINGS)
 
-saveRDS(orig_train_df, "train_cleanformat.rds")
+  # Stringify --------------------
+  str_cols <- c("manufacturer", "group", "content", "unit", "pharmForm",
+    "salesIndex", "category", "campaignIndex")
+  items[, (str_cols) := lapply(.SD, str_to_lower), .SDcols = str_cols]
+
+  # Missing Values --------------------
+  items[, `:=`(
+      pharmForm_isNA = ifelse(is.na(pharmForm), 1, 0)
+      , pharmForm = ifelse(is.na(pharmForm), "-999", pharmForm)
+
+      , category_isNA = ifelse(is.na(category), 1, 0)
+      , category = ifelse(is.na(category), "-999", category)
+
+      , campaignIndex_isNA = ifelse(is.na(campaignIndex), 1, 0)
+      , campaignIndex = ifelse(is.na(campaignIndex), "-999", campaignIndex)
+    )]
+
+  write_feather(items, OUT$items)
+  message(sprintf("Wrote: %s", OUT$items))
+}
+
+
+# ==================== TRAIN & TEST ====================
+clean_lines <- function(in_path, out_path, has_labels = FALSE) {
+  df <- fread(in_path, na.strings = NA_STRINGS)
+
+  str_cols <- c("availability")
+  df[, (str_cols) := lapply(.SD, as.character), .SDcols = str_cols]
+
+  # Labels Only --------------------
+  if (has_labels) {
+    logi_cols <- c("click", "basket", "order")
+    df[, (logi_cols) := lapply(.SD, as.logical), .SDcols = logi_cols]
+  }
+
+  write_feather(df, out_path)
+  message(sprintf("Wrote: %s", out_path))
+}
+
+
+if (!file.exists(OUT$train))
+  clean_lines(IN$train, OUT$train, has_labels = TRUE)
+if (!file.exists(OUT$test))
+  clean_lines(IN$test, OUT$test)
 
