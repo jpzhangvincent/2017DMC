9b9c035cad12003c757b137aecb476a0e7c896b6
rayljazz <jessiewtlin@hotmail.com>
2017-05-13 07:33:18 -0700

    add more interactions

--- src/features/likelihood_generator.R
+++ src/features/likelihood_generator.R
@@ -1,11 +1,75 @@
 
 library(feather)
 
-#setwd('D:/Dropbox/UCDavis/2017spring/DMC_2017_task/github/2017DMC/src')
 
 
-train_set <- read_feather("data/processed/end63_train.feather")
-pesudo_test_set <- read_feather("data/processed/end63_test.feather")
+train_set63 <- read_feather("data/processed/end63_train.feather")
+pesudo_test_set63 <- read_feather("data/processed/end63_test.feather")
+
+train_set77 <- read_feather("data/processed/end77_train.feather")
+pesudo_test_set77 <- read_feather("data/processed/end77_test.feather")
+
+# comnination of manu and group
+train_set63$manu_group = factor(paste(train_set63$manufacturer, 
+                                      train_set63$group,sep='_'))
+pesudo_test_set63$manu_group = factor(paste(pesudo_test_set63$manufacturer, 
+                                            pesudo_test_set63$group,sep='_'))
+
+train_set63$manu_group_label = as.numeric(train_set63$manu_group)
+pesudo_test_set63$manu_group_label = as.numeric(pesudo_test_set63$manu_group)
+
+train_set77$manu_group = factor(paste(train_set77$manufacturer, 
+                                      train_set77$group,sep='_'))
+pesudo_test_set77$manu_group = factor(paste(pesudo_test_set77$manufacturer, 
+                                            pesudo_test_set77$group,sep='_'))
+
+train_set77$manu_group_label = as.numeric(train_set77$manu_group)
+pesudo_test_set77$manu_group_label = as.numeric(pesudo_test_set77$manu_group)
+
+# comnination of content, unit and pharmForm
+train_set63$content_unit_pharmForm = factor(paste(train_set63$content, 
+                                                  train_set63$unit, 
+                                                  train_set63$pharmForm,sep='_'))
+pesudo_test_set63$content_unit_pharmForm = factor(paste(pesudo_test_set63$content, 
+                                                        pesudo_test_set63$unit, 
+                                                        pesudo_test_set63$pharmForm ,sep='_'))
+
+train_set63$content_unit_pharmForm_label =  as.numeric(train_set63$content_unit_pharmForm)
+pesudo_test_set63$content_unit_pharmForm_label = as.numeric(pesudo_test_set63$content_unit_pharmForm)
+
+train_set77$content_unit_pharmForm = factor(paste(train_set77$content, 
+                                                  train_set77$unit, 
+                                                  train_set77$pharmForm,sep='_'))
+pesudo_test_set77$content_unit_pharmForm = factor(paste(pesudo_test_set77$content, 
+                                                        pesudo_test_set77$unit, 
+                                                        pesudo_test_set77$pharmForm ,sep='_'))
+
+train_set77$content_unit_pharmForm_label =  as.numeric(train_set77$content_unit_pharmForm)
+pesudo_test_set77$content_unit_pharmForm_label = as.numeric(pesudo_test_set77$content_unit_pharmForm)
+
+# comnination of day, adflag, availability and campaignIndex
+train_set63$day_adflag_availability_campaignIndex = factor(paste(train_set63$day, 
+                                                                 train_set63$adFlag, 
+                                                                 train_set63$availability, 
+                                                                 train_set63$campaignIndex,sep='_'))
+pesudo_test_set63$day_adflag_availability_campaignIndex = factor(paste(pesudo_test_set63$day, 
+                                                                       pesudo_test_set63$adFlag, 
+                                                                       pesudo_test_set63$availability, 
+                                                                       pesudo_test_set63$campaignIndex,sep='_'))
+
+train_set63$day_adflag_availability_campaignIndex_label =  as.numeric(train_set63$day_adflag_availability_campaignIndex)
+pesudo_test_set63$day_adflag_availability_campaignIndex_label = as.numeric(pesudo_test_set63$day_adflag_availability_campaignIndex)
+
+train_set77$day_adflag_availability_campaignIndex = factor(paste(train_set77$day, train_set77$adFlag, 
+                                                                 train_set77$availability, 
+                                                                 train_set77$campaignIndex,sep='_'))
+pesudo_test_set77$day_adflag_availability_campaignIndex = factor(paste(pesudo_test_set77$day, 
+                                                                       pesudo_test_set77$adFlag, 
+                                                                       pesudo_test_set77$availability, 
+                                                                       pesudo_test_set77$campaignIndex,sep='_'))
+
+train_set77$day_adflag_availability_campaignIndex_label =  as.numeric(train_set77$day_adflag_availability_campaignIndex)
+pesudo_test_set77$day_adflag_availability_campaignIndex_label = as.numeric(pesudo_test_set77$day_adflag_availability_campaignIndex)
 
 
 # likelihood generator function for training dataset
@@ -34,14 +98,61 @@ Likelihood_Train_Generator <- function(X_train, y_train, var_name, noise_sd = 0.
 
 
 #' construct likelihood list
-likelihood_list <- c('pid',"manufacturer",'group')
+likelihood_list <- c("deduplicated_pid",'pid',"manufacturer",'group','pharmForm','salesIndex',
+                     'manu_group','content_unit_pharmForm','day_adflag_availability_campaignIndex')
 
 for(likelihood_term in likelihood_list){
-  LL_train = as.numeric(Likelihood_Train_Generator(train_set, train_set$order, likelihood_term))
-  train_set <- cbind(train_set, LL_train)
-  names(train_set)[names(train_set) == 'LL_train'] = paste(likelihood_term,'likelihood',sep='_')
-  LL_valid = as.numeric(Likelihood_Test_Generator(train_set, train_set$order, pesudo_test_set, likelihood_term))
-  pesudo_test_set <- cbind(pesudo_test_set, LL_valid)
-  names(pesudo_test_set)[names(pesudo_test_set) == 'LL_valid'] = paste(likelihood_term,'likelihood',sep='_')
+  LL_train = as.numeric(Likelihood_Train_Generator(train_set63, train_set63$order, likelihood_term))
+  train_set63 <- cbind(train_set63, LL_train)
+  names(train_set63)[names(train_set63) == 'LL_train'] = paste(likelihood_term,'likelihood',sep='_')
+  LL_valid = as.numeric(Likelihood_Test_Generator(train_set63, train_set63$order, pesudo_test_set63, likelihood_term))
+  pesudo_test_set63 <- cbind(pesudo_test_set63, LL_valid)
+  names(pesudo_test_set63)[names(pesudo_test_set63) == 'LL_valid'] = paste(likelihood_term,'likelihood',sep='_')
 }
 
+
+for(likelihood_term in likelihood_list){
+  LL_train = as.numeric(Likelihood_Train_Generator(train_set77, train_set77$order, likelihood_term))
+  train_set77 <- cbind(train_set77, LL_train)
+  names(train_set77)[names(train_set77) == 'LL_train'] = paste(likelihood_term,'likelihood',sep='_')
+  LL_valid = as.numeric(Likelihood_Test_Generator(train_set77, train_set77$order, pesudo_test_set77, likelihood_term))
+  pesudo_test_set77 <- cbind(pesudo_test_set77, LL_valid)
+  names(pesudo_test_set77)[names(pesudo_test_set77) == 'LL_valid'] = paste(likelihood_term,'likelihood',sep='_')
+}
+
+
+likelihood_end63_train = train_set63[,names(train_set63) %in% c("pid","deduplicated_pid_likelihood","pid_likelihood", 
+                                                                "manufacturer_likelihood","group_likelihood",
+                                                                "pharmForm_likelihood","salesIndex_likelihood",
+                                                                "manu_group_likelihood","content_unit_pharmForm_likelihood"               
+                                                                ,"day_adflag_availability_campaignIndex_likelihood",
+                                                                "manu_group_label","content_unit_pharmForm_label", 
+                                                                "day_adflag_availability_campaignIndex_label")]
+likelihood_end63_test = pesudo_test_set63[,names(pesudo_test_set63) %in% c("pid","deduplicated_pid_likelihood","pid_likelihood", 
+                                                                           "manufacturer_likelihood","group_likelihood",
+                                                                           "pharmForm_likelihood","salesIndex_likelihood",
+                                                                           "manu_group_likelihood","content_unit_pharmForm_likelihood"               
+                                                                           ,"day_adflag_availability_campaignIndex_likelihood",
+                                                                           "manu_group_label","content_unit_pharmForm_label", 
+                                                                           "day_adflag_availability_campaignIndex_label")]
+
+likelihood_end77_train = train_set77[,names(train_set77) %in% c("pid","deduplicated_pid_likelihood","pid_likelihood", 
+                                                                "manufacturer_likelihood","group_likelihood",
+                                                                "pharmForm_likelihood","salesIndex_likelihood",
+                                                                "manu_group_likelihood","content_unit_pharmForm_likelihood"               
+                                                                ,"day_adflag_availability_campaignIndex_likelihood",
+                                                                "manu_group_label","content_unit_pharmForm_label", 
+                                                                "day_adflag_availability_campaignIndex_label")]
+likelihood_end77_test = pesudo_test_set77[,names(pesudo_test_set77) %in% c("pid","deduplicated_pid_likelihood","pid_likelihood", 
+                                                                           "manufacturer_likelihood","group_likelihood",
+                                                                           "pharmForm_likelihood","salesIndex_likelihood",
+                                                                           "manu_group_likelihood","content_unit_pharmForm_likelihood"               
+                                                                           ,"day_adflag_availability_campaignIndex_likelihood",
+                                                                           "manu_group_label","content_unit_pharmForm_label", 
+                                                                           "day_adflag_availability_campaignIndex_label")]
+
+write_feather(likelihood_end63_train, 'data/processed/likelihood_end63_train.feather')
+write_feather(likelihood_end63_test, 'data/processed/likelihood_end63_test.feather')
+
+write_feather(likelihood_end77_train, 'data/processed/likelihood_end77_train.feather')
+write_feather(likelihood_end77_test, 'data/processed/likelihood_end77_test.feather')
