1588d3cd9178f124ed34419e0693dd20a6aa15a2
Nick Ulle <nick.ulle@gmail.com>
2017-05-14 18:48:11 -0700

    Update PMI script.

--- src/3.2_pmi_features.py
+++ src/3.2_pmi_features.py
@@ -18,10 +18,10 @@ import pandas as pd
 
 def main():
     for path in glob.glob("../data/interim/3_*train.feather"):
-        write_pmi(path)
+        write_pmi(path, "group")
 
 
-def write_pmi(path):
+def write_pmi(path, by):
     print("Reading: %s" % path)
     train = feather.read_dataframe(path)
     train.pid = train.pid.astype(str)
@@ -33,36 +33,29 @@ def write_pmi(path):
     # Set up the path to the out directory.
     prefix = path.rsplit("_", 1)[0]
     prefix = prefix.replace("interim", "merge")
+    prefix = prefix.replace("3_", "pmi_")
 
-
-    # By pid ----------------------------------------
     by = "pid"
-    pmi_pid = compute_pmi(train, by)
-
-    path = prefix + "_pmi_%s.feather" % by
-    feather.write_dataframe(pmi_pid, path)
-    print("Wrote: %s" % path)
-    
-    # By group ----------------------------------------
-    by = "group"
-    pmi_group = compute_pmi(train, by)
-
-    path = prefix + "_pmi_%s.feather" % by
-    feather.write_dataframe(pmi_group, path)
-    print("Wrote: %s" % path)
-
-
-def compute_pmi(runs, group):
-    runs = runs.apply(lambda x: sorted(x[group].tolist()))
+    runs = train.apply(lambda x: sorted(x[by].tolist()))
     runs = runs.tolist()
 
+    del train
+
+    pmi_pid = compute_pmi(runs, by)
+
+    path = prefix + "_%s.feather" % by
+    feather.write_dataframe(pmi_pid, path)
+    print("Wrote: %s" % path)
+
+
+def compute_pmi(runs, by):
     pair_counts = co.Counter(c for r in runs for c in it.combinations(r, 2))
     counts = co.Counter(x for r in runs for x in r)
 
     # Compute pointwise mutual information (PMI).
     pmi = pd.DataFrame({
             "group": x + "/" + y,
-            "pmi_" + group: np.log(pair_counts[(x, y)] / counts[x] * counts[y])
+            "pmi_" + by: np.log(pair_counts[(x, y)] / counts[x] * counts[y])
         } for x, y in pair_counts.keys())
 
     return pmi
