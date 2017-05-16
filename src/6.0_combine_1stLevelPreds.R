ad2ce11b19895860feb17794213f38b305480977
Nick Ulle <nick.ulle@gmail.com>
2017-05-15 19:34:47 -0700

    Add layer 1 merge script.

--- src/6.0_combine_1stLevelPreds.R
+++ src/6.0_combine_1stLevelPreds.R
@@ -4,19 +4,52 @@ library(data.table)
 library(feather)
 
 
-IN = c(
-  train = "../data/processed/end%i_train.feather"
-  , test = "../data/processed/end%i_test.feather")
-
-
-PRED_DIR = "../data/preds1stLevel"
-
-
 main = function() {
-  paths = list.files(PRED_DIR, "train77")
+  message()
+  DAYS = c(63, 77, 92)
 
-  train = data.table(read_feather())
-  test = data.table(read_feather())
+  message("---------------------------------------- Train")
+  lapply(DAYS, merge_set, "train")
+
+  message("---------------------------------------- Test")
+  lapply(DAYS, merge_set, "test")
+
+  message("All done.\n")
 }
 
-#main()
+
+merge_set = function(d, set) {
+  # Load the relevant data set.
+  IN = "../data/processed/end%i_%s.feather"
+
+  data_path = sprintf(IN, d, set)
+  df = data.table(read_feather(data_path))
+  message(sprintf("Read: %s", data_path))
+  setkey(df, lineID)
+
+  # Merge every matching file in the prediction directory.
+  PRED_PATTERN = "^end%i_%s" # preds files start `end##_SET_`
+  PRED_DIR = "../data/preds1stLevel"
+
+  re = sprintf(PRED_PATTERN, d, set)
+  paths = list.files(PRED_DIR, re, full.names = TRUE)
+  for (path in paths) {
+    message(sprintf("  Merging: %s", path))
+    pred = data.table(read_feather(path))
+    setkey(pred, lineID)
+
+    df = merge(df, pred, all.x = TRUE)
+  }
+
+  # Write
+  OUT = "../data/layer2/end%i_%s_layer2.feather"
+
+  out_path = sprintf(OUT, d, set)
+  write_feather(df, out_path)
+  message(sprintf("  WROTE: %s\n", out_path))
+
+  invisible (NULL)
+}
+
+
+main()
