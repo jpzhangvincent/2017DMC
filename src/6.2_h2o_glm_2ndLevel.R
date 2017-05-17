fa02133749d463c215aaae6946a027bb31f21f92
GinnyZheng <jgzheng@ucdavis.edu>
2017-05-16 20:23:01 -0700

    glm 2nd level

--- src/6.2_h2o_glm_2ndLevel.R
+++ src/6.2_h2o_glm_2ndLevel.R
@@ -5,7 +5,8 @@ library(feather)
 library(stringr)
 library(h2o)
 
-h2o.init(nthreads = 36) #Number of threads -1 means use all cores on your machine  #max mem size is the maximum memory to allocate to H2O
+h2o.init(nthreads = -1, #Number of threads -1 means use all cores on your machine
+         max_mem_size = "20G")  #max mem size is the maximum memory to allocate to H2O
 h2o.removeAll()
 
 ####################################################################
@@ -15,8 +16,48 @@ h2o.removeAll()
 train63d <- read_feather("../data/processed/end63_train_2nd.feather")
 valid63d <- read_feather("../data/processed/end63_test_2nd.feather")
 
+train63d <- read_feather("end63_train_layer2_DEMO.feather")
+valid63d <- read_feather("end63_test_layer2_DEMO.feather")
+
+offset_logrithm = function( data_set){
+  # create another revenue column called log_revenue
+  data_set$log_revenue = log(1e-5+ data_set$revenue) - log(1e-5)
+  data_set$loo_mean_revenue_by_pid = abs(1e-5+data_set$loo_mean_revenue_by_pid) - log(1e-5)
+  
+  # all columns need to transfer to log-scale
+  need_to_log=c("avg_price_basket_info",
+                "avg_price_click_info",
+                "avg_price_order_info",
+                "avg_revenue_by_group_10",
+                "avg_revenue_by_group_30",
+                "avg_revenue_by_group_7",
+                "competitorPrice_per_unit",
+                "loo_mean_revenue_by_pid",
+                "next_price",
+                "next5_price_avg",
+                "next5_price_max",
+                "next5_price_min",
+                "prev_price",
+                "prev5_price_avg",
+                "prev5_price_diff",
+                "prev5_price_max",
+                "prev5_price_min",
+                "price",
+                "price_per_unit",
+                "rrp",
+                "rrp_per_unit")
+  
+  data_set[,need_to_log] = log(1e-5+ data_set[,need_to_log]) - log(1e-5)
+  
+  return(data_set)
+}
+
+newtrain63d = offset_logrithm(train63d)
+newvalid63d = offset_logrithm(valid63d)
+
+
 # define predictors from original features
-features <- fread("../data/feature_list.csv")
+features <- fread("feature_list.csv")
 #treat day_mod_ features as categorical
 features[str_detect(name,'day_mod_'),type := "categorical"]
 #should not include them in the modeling
@@ -42,14 +83,15 @@ if (REPLACE_HIGH_DIMENSION_VARS == TRUE){
 #1st level predictions features(meta-model features)
 column_names <- names(train63d)
 model_features <- column_names[str_detect(column_names, 'preds_')]
+model_features = model_features[1:3]
 
-label <- c("revenue")
+label <- c("log_revenue")
 # combine all features
 all_preds <- c(cat_vars, cont_vars, model_features)
 all_vars <- c(all_preds, label)
 
-train_set.hex <- as.h2o(train63d[all_vars])
-validation_set.hex <- as.h2o(valid63d[all_vars])
+train_set.hex <- as.h2o(newtrain63d[all_vars])
+validation_set.hex <- as.h2o(newvalid63d[all_vars])
 rm(train63d, valid63d)
 
 # factorize the categorical variables
@@ -66,8 +108,8 @@ for (c in cat_vars) {
 ####################################################################
 
 # GLM hyperparamters
-glm_params <- list( alpha = c(1e-2, 0.1,0.15,0.2,0.25,0.3,0.35,0.5,0.8,0.85,0.9),
-                    lambda = c(1e-4, 1e-2,1e-3, 0.1, 0.15,0.2,0.25,0.5,0.8,0.85,0.9,0.95))
+glm_params <- list( alpha = c(0.1,0.5,0.9),
+                    lambda = c(0.1,0.5,0.9))
 
 # Random Grid Search
 search_criteria <- list(strategy = "RandomDiscrete", 
@@ -78,14 +120,14 @@ search_criteria <- list(strategy = "RandomDiscrete",
                         ## early stopping once the leaderboard of the top 5 models is 
                         #converged to 0.1% relative difference
                         stopping_rounds = 5,                
-                        stopping_metric = "AUC",
+                        stopping_metric = "MSE",
                         stopping_tolerance = 1e-3)
 
 # Train and validate a grid of glms for parameter tuning
 # tuning parameter p = c(0,1,1.3,1.5,1.8,2,3,4)
 glm_grid <- h2o.grid(algorithm = "glm",
                      family = "tweedie",
-                     tweedie_variance_power = 1.3,
+                     tweedie_variance_power = 1.1,
                      hyper_params = glm_params,
                      search_criteria = search_criteria,
                      x = all_preds, 
@@ -95,7 +137,7 @@ glm_grid <- h2o.grid(algorithm = "glm",
                      validation_frame = validation_set.hex)
 
 sorted_GLM_Grid <- h2o.getGrid(grid_id = "glm_grid", 
-                               sort_by = "auc", 
+                               sort_by = "rmse", 
                                decreasing = TRUE)
 print(sorted_GLM_Grid)
 # remove the data in h2o
@@ -159,4 +201,63 @@ for (i in 1:3) {
   write_feather(preds_train77d, paste0("../data/preds1stLevel/h2o_glm_train77d-",retrained_glm_auc,'.feather'))
   write_feather(preds_test77d, paste0("../data/preds1stLevel/h2o_glm_test77d-",retrained_glm_auc,'.feather'))
 }
+####################################################################
+### Retain the model on train92d                                 ###
+####################################################################
+#Load train92d and test92d dataset
+train92d <- read_feather("../data/processed/end92_train.feather")
+test92d <- read_feather("../data/processed/end92_test.feather")
+test92d = test92d[,-84]
+
+train92d_index_df <- train92d[c("lineID")]
+test92d_index_df <- test92d[c("lineID")]
+
+#Load into the h2o environment
+retrain_set.hex <- as.h2o(train92d[all_vars])
+test_set.hex <- as.h2o(test92d[all_preds])
+
+# factorize the categorical variables
+for(c in cat_vars){
+  retrain_set.hex[c] <- as.factor(retrain_set.hex[c])
+}
+
+for(c in cat_vars){
+  test_set.hex[c] <- as.factor(test_set.hex[c])
+}
+
+
+# Only choose the top 3 models and persist the retrained model
+# Note: need to refit model including the pesudo validation set
+for (i in 1:3) {
+  glm <- h2o.getModel(sorted_GLM_Grid@model_ids[[i]])
+  retrained_glm <- do.call(h2o.glm,
+                           ## update parameters in place
+                           {
+                             p <- glm@parameters        # the same seed
+                             p$model_id = NULL          ## do not overwrite the original grid model
+                             p$training_frame = retrain_set.hex   ## use the full training dataset
+                             p$validation_frame = NULL  ## no validation frame
+                             p
+                           }
+  )
+  print(glm@model_id)
+  ## Get the AUC on the hold-out test set
+  #retrained_glm_auc <- round(h2o.auc(h2o.performance(retrained_glm, newdata = test_set.hex)),4)
+  #print(paste0("The AUC on 77-92 days: ", retrained_glm_auc))
+  preds_train92d <- as.data.frame(h2o.predict(retrained_glm, retrain_set.hex))[,3]
+  preds_test92d <- as.data.frame(h2o.predict(retrained_glm, test_set.hex))[,3]
+  preds_train92d <- cbind(train92d_index_df, preds_train92d)
+  preds_test92d <- cbind(test92d_index_df, preds_test92d)
+  newnames = paste("preds_glm",i,sep="")
+  names(preds_train92d)[2] = newnames
+  names(preds_test92d)[2] = newnames
+  
+  # save the retrained model to regenerate the predictions for 2nd level modeling 
+  # and possibly useful for ensemble
+  #h2o.saveModel(retrained_glm, paste("../models/1stLevel/end92d_h2o_glm",retrained_glm_auc,sep = '-'), force = TRUE)
+  write_feather(preds_train92d, paste0("../data/preds1stLevel/end92_train_glm",i,'.feather'))
+  write_feather(preds_test92d, paste0("../data/preds1stLevel/end92_test_glm",i,'.feather'))
+}
+
 h2o.shutdown(prompt = FALSE)
+
