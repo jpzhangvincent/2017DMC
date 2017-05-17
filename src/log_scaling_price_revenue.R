fb5e6b7e2ae9b3cc84843cde1f4a5a57a41d4468
HaoranLi <hrli@ucdavis.edu>
2017-05-16 19:27:24 -0700

    Update log_scaling_price_revenue.R
    offset logarithm transfer.

--- src/log_scaling_price_revenue.R
+++ src/log_scaling_price_revenue.R
@@ -1,38 +1,4 @@
-order1_logarithm = function( data_set){
-        tmp = subset( data_set, order == 1 )
-        
-        tmp$log_revenue = log(tmp$revenue)
-        tmp$loo_mean_revenue_by_pid = abs(tmp$loo_mean_revenue_by_pid)
-        
-        need_to_log=c("avg_price_basket_info",
-        "avg_price_click_info",
-        "avg_price_order_info",
-        "avg_revenue_by_group_10",
-        "avg_revenue_by_group_30",
-        "avg_revenue_by_group_7",
-        "competitorPrice_per_unit",
-        "loo_mean_revenue_by_pid",
-        "next_price",
-        "next5_price_avg",
-        "next5_price_max",
-        "next5_price_min",
-        "prev_price",
-        "prev5_price_avg",
-        "prev5_price_diff",
-        "prev5_price_max",
-        "prev5_price_min",
-        "price",
-        "price_per_unit",
-        "rrp",
-        "rrp_per_unit")
-        
-        tmp[,need_to_log] = log(tmp[,need_to_log])
-        return(tmp)
-}
-
-
-
-offset_logarithm = function( data_set){
+offset_logrithm = function( data_set){
        
         tmp$log_revenue = log(1e-5+ tmp$revenue)
         tmp$loo_mean_revenue_by_pid = abs(1e-5+tmp$loo_mean_revenue_by_pid)
@@ -63,4 +29,5 @@ offset_logarithm = function( data_set){
         
         return(tmp)
 }
+
 
